8a5a5bc8730647c790ebe51c4ad191bb14d3f42d
Ian Ehlert <ehlertij@gmail.com>
2014-07-09 11:03:36 -0500

    fix dependency cookbook name

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ian.ehlert@sportngin.com"
 license          "Apache"
 description      "Installs elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.1.0"
+version          "0.2.0"
 
-depends "java"
+depends "opsworks_java::setup"
