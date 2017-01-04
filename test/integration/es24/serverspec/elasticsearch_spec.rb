e056fea025dfdfd7409d0523b80ee8fb0db23c0b
Tim Sandquist <tim.sandquist@sportsengine.com>
2017-01-04 11:26:11 -0600

    Update to allow installation of Elasticsearch 2.x

---
+++ test/integration/es24/serverspec/elasticsearch_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe service('monit') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('elasticsearch') do
+  it { should be_running }
+end
+
+describe process('bash /etc/init.d/elasticsearch') do
+  it { should_not be_running }
+end
+
+describe command('curl -s localhost:9200') do
+  its(:stdout) { should include '"number" : "2.4.3",'}
+  its(:stdout) { should include '"tagline" : "You Know, for Search"'}
+end
+
+describe file('/etc/init.d/elasticsearch') do
+  it { should be_file }
+  its(:content) { should include 'elasticsearch' }
+end
+
+describe file('/etc/monit.d/elasticsearch.monitrc') do
+  it { should be_file }
+  its(:content) { should include 'elasticsearch' }
+end
+
+describe file('/data/elasticsearch/config/elasticsearch-env.sh') do
+  it { should be_file }
+  its(:content) { should include 'elasticsearch' }
+end
+
+describe file('/data/elasticsearch/config/elasticsearch.yml') do
+  it { should be_file }
+  its(:content) { should include 'elasticsearch' }
+  its(:content) { should match /^cluster.name: elasticsearch-cluster/ }
+  its(:content) { should match /^node.name: "es1"/ }
+  its(:content) { should match /^node.master: true/ }
+  its(:content) { should match /^node.data: true/ }
+  its(:content) { should match /^discovery.zen.minimum_master_nodes: 1/ }
+  its(:content) { should match /^discovery.zen.ping.unicast.hosts: \["127.0.0.1"\]/ }
+end
